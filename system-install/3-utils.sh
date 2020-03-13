dd67e8c37ce4685058f6bbede43f76b24752971d
Evgeny Rozaliev <rozaliev@gmail.com>
2020-03-14 00:18:34 +0300

    add cargo edit

--- system-install/3-utils.sh
+++ system-install/3-utils.sh
@@ -2,7 +2,7 @@
 
 pamac install bat fd hexyl ttf-fira-code
 
-cargo install broot procs exa bingrep starship
+cargo install broot procs exa bingrep starship cargo-edit
 
 cargo install -f --git https://github.com/cjbassi/ytop ytop
 cargo install -f --git https://github.com/bootandy/dust
