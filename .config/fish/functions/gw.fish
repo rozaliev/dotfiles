acecd11fa482deb2a0a2ed556cc969be807ba362
Evgeny Rozaliev <rozaliev@gmail.com>
2020-03-28 22:01:54 +0300

    change mout path

--- .config/fish/functions/gw.fish
+++ .config/fish/functions/gw.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function gw --wraps='cd /run/media/erz/Big/Work' --description 'alias gw=cd /run/media/erz/Big/Work'
-  cd /run/media/erz/Big/Work $argv;
+function gw --wraps='cd /run/media/system/Big/Work' --description 'alias gw=cd /run/media/system/Big/Work'
+  cd /run/media/system/Big/Work $argv;
 end
